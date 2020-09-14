8fcfe41fd53b60b19963193b1b8282f12dd9c243
Roman Rajchert <rajchert.roman@gmail.com>
2020-09-14 17:59:49 +0200

    Connect Redux and Sagas

---
+++ src/store.ts
@@ -0,0 +1,21 @@
+import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+import reducer from './reducers';
+import sagas from './sagas';
+
+
+// create the saga middleware
+const sagaMiddleware = createSagaMiddleware();
+// mount it on the Store
+const store = createStore(
+	reducer,
+  composeWithDevTools(applyMiddleware(sagaMiddleware))
+);
+
+// run the saga
+sagaMiddleware.run(sagas);
+
+
+export default store;
