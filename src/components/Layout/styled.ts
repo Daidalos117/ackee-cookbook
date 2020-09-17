e2cf7db0a5da653864a4d87fe7cfdfa15748530e
Roman Rajchert <rajchert.roman@gmail.com>
2020-09-17 22:43:55 +0200

    Add transient styled component prop

--- src/components/Layout/styled.ts
+++ src/components/Layout/styled.ts
@@ -2,8 +2,8 @@ import styled from "styled-components";
 import Container from "@material-ui/core/Container/Container";
 
 export interface StyledContainerProps {
-  marginTop: number | boolean;
+    $marginTop: number | boolean;
 }
 export const StyledContainer = styled(Container)<StyledContainerProps>`
-    margin-top: ${({marginTop}) => marginTop ? '55px' : '0'};
+    margin-top: ${({ $marginTop }) => ($marginTop ? "55px" : "0")};
 `;
